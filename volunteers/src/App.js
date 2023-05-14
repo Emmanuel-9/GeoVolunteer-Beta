b15608b80ff767a1dd9008717c06398b124cd48d
Bratipah (she/her) <57480729+Bratipah@users.noreply.github.com>
2023-05-14 20:03:20 +0300

    Update App.js

--- volunteers/src/App.js
+++ volunteers/src/App.js
@@ -1,49 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Dustbin = require("./models/dustbin");
 
-import { useAuth } from './hooks/auth-hook';
-import { AuthContext } from './context/auth-context';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+const app = express();
 
-function App() {
-  const { token, login, logout, userId } = useAuth();
-  let routes;
-  if (userId) {
-    routes = (
-      <Switch>
-        <Route
-          path="/volunteer" exact>
-                    <Volunteer />
-        </Route>
-        <Route
-          path="/monitoring" exact>
-                    <Monitoring />
-        </Route>                
-      </Switch>
+const adminRoutes = require('./routes/admin');
+const dustbinRoutes = require('./routes/dustbin');
+const customerRoutes = require('./routes/customer');
+const driverRoutes = require('./routes/driver');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const dbUrl = process.env.DB_LINK;
+mongoose.connect(dbUrl, { keepAlive: 1, useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        console.log('connected to db!');
+    })
+    .catch((err) => {
+        console.log('not connected to db due to the following error: ' + err);
+    });
+
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    res.setHeader(
+        "Access-Control-Allow-Methods", 
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
 
-export default App;
+
+// Routes for actors
+app.use('/api/admin', adminRoutes);
+app.use('/api/dustbin', dustbinRoutes);
+app.use('/api/customer', customerRoutes);
+app.use('/api/driver', driverRoutes);
+
+
+// test for communication from hardware
+app.post('/test/test', (req, res) => {
+    console.log('Status = ' + req.body.level);
+    console.log('Dustbin ID = ' + req.body.id);
+    res.status(200).json({
+        'message': 'Request Reached!'
+    });
+    Dustbin.findOneAndUpdate({ id: req.body.id }, { status: parseInt(req.body.level) })
+        .then((resp) => {
+            console.log('Updated');
+        })
+        .catch((err) => {
+            console.log('cannot update due to error: ' + err);
+        });
+});
+
+// test for server online
+app.get('/test/test', (req, res) => {
+    res.json({
+        message: "Server is online!"
+    });
+})
+
+// returning API key for Google maps API 
+app.get('/get/api_key', (req, res) => {
+    res.status(200).json({
+        api_Key: process.env.API_KEY
+    });
+});
+
+
+
+module.exports = app;
